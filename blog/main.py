e20422ea0222186473e6adc94fbfe12b7c5ec9f7
JoniMonetti1 <158229526+JoniMonetti1@users.noreply.github.com>
2025-06-16 01:19:19 -0300

    creating crud methods and adding http responses

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Response, HTTPException
 
 from . import schemas, models
-from .database import engine
+from .database import engine, SessionLocal
+
+from sqlalchemy.orm import Session
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
-@app.post("/blog")
-def create_blog(request: schemas.Blog):
-    return request
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+@app.post("/blog", status_code = 201)
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get("/blog")
+def get_all_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get("/blog/{blog_id}", status_code = 200)
+def get_blog_by_id(
+        blog_id: int,
+        response: Response,
+        db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
+    if not blog:
+        raise HTTPException(status_code = 404, detail = f"Blog with id:{blog_id} not found")
+        # response.status_code = 404
+        # return {"error": f"Blog with id:{blog_id} not found"}
+    return blog
