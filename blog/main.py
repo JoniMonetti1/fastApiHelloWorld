e9ed539f90ac383ece60e2a0fe46d70d9a4299d5
JoniMonetti1 <158229526+JoniMonetti1@users.noreply.github.com>
2025-06-16 12:24:28 -0300

    CRUD methods complete

--- blog/main.py
+++ blog/main.py
@@ -22,6 +22,38 @@ def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete("/blog/{blog_id}", status_code = 204)
+def delete_blog(blog_id: int, db: Session = Depends(get_db)):
+    blog_query = db.query(models.Blog).filter(models.Blog.id == blog_id)
+    blog = blog_query.first()
+
+    if not blog:
+        raise HTTPException(status_code = 404, detail = f"Blog with id:{blog_id} not found")
+
+    blog_query.delete(synchronize_session=False)
+
+    db.commit()
+
+    return {"detail": f"Blog with id:{blog_id} deleted successfully"}
+
+@app.put("/blog/{blog_id", status_code = 202)
+def update_blog(
+        blog_id: int,
+        request: schemas.Blog,
+        db: Session = Depends(get_db)):
+    blog_query = db.query(models.Blog).filter(models.Blog.id == blog_id)
+    blog = blog_query.first()
+
+    if not blog:
+        raise HTTPException(status_code = 404, detail = f"Blog with id:{blog_id} not found")
+
+    blog_query.update(request.model_dump(), synchronize_session=False)
+
+    db.commit()
+    db.refresh(blog)
+
+    return blog
+
 @app.get("/blog")
 def get_all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
